3b6e48f6c5b90b0c418a7d27146d7e9bb7c086b0
Alexandra <102197498+AlexandraRodCas@users.noreply.github.com>
2023-02-22 19:22:19 -0700

    botones funcionando

--- app/src/main/java/alexandra/rodriguez/thecheezery/MenuActivity.kt
+++ app/src/main/java/alexandra/rodriguez/thecheezery/MenuActivity.kt
@@ -1,11 +1,37 @@
 package alexandra.rodriguez.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnCold:Button = findViewById(R.id.button_cold_drinks)
+        btnCold.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        var btnHot:Button = findViewById(R.id.button_hot_drinks)
+        btnHot.setOnClickListener {
+            var intent: Intent = Intent(this,HotdrinksActivity::class.java)
+            startActivity(intent)
+        }
+
+        var btnSweets:Button = findViewById(R.id.button_sweets)
+        btnSweets.setOnClickListener {
+            var intent: Intent = Intent(this,SweetsActivity::class.java)
+            startActivity(intent)
+        }
+
+        var btnSalties:Button = findViewById(R.id.button_salties)
+        btnSalties.setOnClickListener {
+            var intent: Intent = Intent(this,SaltiesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/alexandra/rodriguez/thecheezery/Product.kt
@@ -0,0 +1,6 @@
+package alexandra.rodriguez.thecheezery
+
+data class Product (var name:String,
+                    var image:Int,
+                    var descrption:String,
+                    var price:Double)
